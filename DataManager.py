47c7bf6faec44dfd31c69d484d544aa634539dda
William Tichenor <williamtichenor@tamu.edu>
2024-10-22 22:06:22 -0500

    bow processing

--- DataManager.py
+++ DataManager.py
@@ -1,15 +1,65 @@
 import pickle
+import pandas as pd
+import ast
+import re
+
+def data_setup():
+    df = pd.read_csv('data/ProjectData.csv', encoding='utf8')
+    df.tail(2)
+    data = []
+
+    #enum
+    TEXT = 0
+    BOW = 1
+    TAGS = 2
+    FOLLOWERS = 3
+    FRIENDS = 4
+    VERIFICATION = 5
+    USERNAME = 6
+    DATE = 7
+
+    for i, row in df.iterrows():
+        txt = row['text']
+        txt = txt.lower()
+        txt = txt.replace('\n', ' ')
+        txt = re.sub(r'[^a-z0-9\s]', '', txt)
+        dict = {}
+        for w in txt.split():
+            if w in dict:
+                dict[w] += 1
+            else:
+                dict[w] = 1
+        try:
+            data.append([
+                row['text'], 
+                dict, 
+                safe_literal_eval(row['hashtags']), 
+                int(row['user_followers']), 
+                int(row['user_friends']), 
+                (True if (row['user_verified'] == "True") else False), 
+                row['user_name'], 
+                row['date']
+            ])
+        except:
+            print("Line",i,"invalid")
+    saveFile = open("index", 'wb')
+    pickle.dump(data, saveFile)
+    saveFile.close
+    print("data ready!")
+
+    
+
+def safe_literal_eval(val):
+    try:
+        return ast.literal_eval(val)
+    except (ValueError, SyntaxError):
+        return []
 
 # Main method only for testing! this won't exist.
 if __name__ == "__main__":
-    """
-    print("hi!")
-    arr = ["asdf", "testing", 5]
-    saveFile = open("index", 'ab')
-    pickle.dump(arr, saveFile)
-    saveFile.close
-    """
-    saveFile = open("index", 'rb')
-    testing = pickle.load(saveFile)
-    print(testing)
+    #data_setup()
+
+    file = open("index", 'rb')
+    testing = pickle.load(file)
+    file.close()
     print(testing[1])
